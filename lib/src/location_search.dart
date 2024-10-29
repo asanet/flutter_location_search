bd5fed71ca533812644ba744ee0762aec089023b
Ataíde Neto <40701906+asanet@users.noreply.github.com>
2024-10-28 21:30:59 -0500

    Update location_search.dart

--- lib/src/location_search.dart
+++ lib/src/location_search.dart
@@ -495,6 +495,7 @@ class _LocationSearchWidgetState extends State<LocationSearchWidget> {
   Widget _buildDialog() {
     return Dialog(
       backgroundColor: Colors.white,
+      surfaceTintColor: Colors.white,
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(15)),
       ),
